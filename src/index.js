6e771471d96812bd7178536f890c869666c8ef63
MGIC12 <mager.infante@gmail.com>
2026-05-28 19:56:58 -0300

    Configuraciones a Prisma y añadido el seed para alimentar la bdd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from "express";
+// import itemRoutes from "./routes/item.routes.js";
 
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
@@ -9,3 +12,5 @@ app.listen(3000, () => {
 app.get("/api/health", (req, res) => {
   res.send({ status: "ok", message: "API funcionando correctamente" });
 });
+
+// app.use("/api/items", itemRoutes);
